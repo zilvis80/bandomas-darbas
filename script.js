c8b872c1362418bbc2679f42266aabf47ba3b625
zilvinas <zilvis80@mail.ru>
2024-01-03 22:56:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,122 @@
+//
+let vardas = 'Zilvinas';
+let pavarde = 'Imbrasas';
+let amzius = 43;
+let ugis = 183;
+let svoris = 83;
+let AM = 'VSC';
+let AGK = 'S4';
+let kurasas = 'pirmas';
+let SPP = 'programuotojas';
+let AKS = 15;
+let AG = 'ifzm-6';
+let Vid = 8;
+let pavadinimas = 'Vilnius';
+let valstybe = 'Torontas';
+let apskritis = 'Vilniaus raj.';
+let ID = 1410;
+let meras = 'Zuokas';
+let PKK = 1230;
+let GS = 3500000;
+
+console.log('Vardas:', vardas);
+console.log('Pavarde:', pavarde);
+console.log('Amzius:', amzius);
+console.log('Ugis:', ugis);
+console.log('Svoris:', svoris);
+console.log('Aukstoji mokykla', AM);
+console.log('Akademines grupes kodas:', AGK);
+console.log('Kuras:', kurasas);
+console.log('Studiju programos pavadinimas:', SPP);
+console.log('Atsiskaitytu kreditu skaicius:', AKS);
+console.log();
+
+console.log('Pavadinimas', pavadinimas);
+console.log('Valstybe', valstybe);
+console.log('Apskritis', apskritis);
+console.log('Ikurimo data', ID);
+console.log('Meras', meras);
+console.log('Plotas Kv.Km.', PKK);
+console.log('Gyventoju skaicius', GS);
+console.log(vardas, '\n', vardas, '\n', vardas, '\n', vardas, '\n', vardas);
+console.log();
+let gyvunas = 'suo';
+let amziu = '2 m.';
+let spalva = 'ruda';
+let svori = '1,4 kg';
+console.log(
+  'Gyvunas',
+  '-',
+  gyvunas,
+  amziu,
+  'turi',
+  spalva,
+  'kailio',
+  'spalvos',
+  'ir',
+  'sveria',
+  svori
+);
+let x = 25;
+console.log('isvedimas', '-', x, x, x, x, x);
+
+let s = '*';
+
+console.log(
+  s +
+    ' ' +
+    s +
+    ' ' +
+    s +
+    ' ' +
+    s +
+    '\n' +
+    s +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    s +
+    '\n' +
+    s +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    ' ' +
+    s +
+    '\n' +
+    s +
+    ' ' +
+    s +
+    ' ' +
+    s +
+    ' ' +
+    s
+);
+
+let a = 4;
+let b = 5;
+
+console.log('suma:' + (a + b));
+console.log('atimtis:' + (a - b));
+
+a = 1;
+b = 4;
+let c = 6;
+
+console.log('sandauga:' + (-a + b * c));
+
+a = 35;
+b = 5;
+c = 7;
+
+console.log('atsakymas:' + ((a + b) % c));
+
+a = 14;
+b = -4;
+c = 6;
+let d = 12;
+
+console.log('ATS:' + a + (b * c) / d);
